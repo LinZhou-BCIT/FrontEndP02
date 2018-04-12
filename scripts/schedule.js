608c1c8f28c952f29588736e66f846b8074689b1
Lin Zhou <lzhou58@my.bcit.ca>
2018-04-12 15:21:21 -0700

    shcedule grid

--- scripts/schedule.js
+++ scripts/schedule.js
@@ -17,8 +17,9 @@ function generateSchedule(data){
     let currentMonthNum = -1;
     let currentMonthTxt = null;
     let currentMonthNode = null;
+    let scheduleGridNode = null;
     for(let day of data) {
-        console.log(day);
+        // console.log(day);
         let date = new Date(day.date);
         // if it's a new month
         let monthNum = date.getMonth();
@@ -27,6 +28,7 @@ function generateSchedule(data){
             currentMonthTxt = monthNames[monthNum];
             currentMonthNode = document.createElement('div');
             currentMonthNode.className = currentMonthTxt;
+            currentMonthNode.classList.add("month");
 
             // month title
             let monthTitleNode = document.createElement('h2');
@@ -34,21 +36,27 @@ function generateSchedule(data){
 
             // day of the week labels
             let daysOfWeekNode = document.createElement('div');
-            daysOfWeekNode.className = "days-of-week-label-container";
+            daysOfWeekNode.className = "doW-label-container";
             for (let dayName of dayNames) {
                 let labelNode = document.createElement('div');
+                labelNode.className = "doW-label";
                 labelNode.appendChild(document.createTextNode(dayName));
                 daysOfWeekNode.appendChild(labelNode);
             }
 
+            // new grid div
+            scheduleGridNode = document.createElement('div');
+            scheduleGridNode.className = "grid";
+
             currentMonthNode.appendChild(monthTitleNode);
             currentMonthNode.appendChild(daysOfWeekNode);
+            currentMonthNode.appendChild(scheduleGridNode);
 
             // grid placeholder divs
             for (let i = 1; i < date.getDay(); i++) {
                 let placeholderNode = document.createElement('div');
                 placeholderNode.className = "grid-placeholder";
-                currentMonthNode.appendChild(placeholderNode);
+                scheduleGridNode.appendChild(placeholderNode);
             }
             rootNode.appendChild(currentMonthNode);
         }
@@ -57,18 +65,23 @@ function generateSchedule(data){
         let dateStr = currentMonthTxt + ' ' + date.getDate() + ', ' + date.getFullYear();
         let dateSpan = document.createElement('span');
         dateSpan.appendChild(document.createTextNode(dateStr));
+        dateSpan.className = "date";
         let courseSpan = document.createElement('span');
         courseSpan.appendChild(document.createTextNode(day.course));
+        courseSpan.className = "course";
         let notesSpan = document.createElement('span');
         notesSpan.appendChild(document.createTextNode(day.notes));
+        notesSpan.className = "notes";
         let instructorSpan = document.createElement('span');
-        instructorSpan.appendChild(document.createTextNode(day.instructorSpan));
+        instructorSpan.appendChild(document.createTextNode(day.instructor));
+        instructorSpan.className = "instructor";
 
         dayNode.appendChild(dateSpan);
         dayNode.appendChild(courseSpan);
         dayNode.appendChild(notesSpan);
         dayNode.appendChild(instructorSpan);
+        dayNode.className = "day";
 
-        currentMonthNode.appendChild(dayNode);
+        scheduleGridNode.appendChild(dayNode);
     }
 }
